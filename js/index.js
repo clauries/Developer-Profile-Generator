e14cdcee8c8517082da03572746b0b22312441fb
clauries <55369403+clauries@users.noreply.github.com>
2019-12-20 14:10:54 -0800

    Added inquirer prompts

--- js/index.js
+++ js/index.js
@@ -4,13 +4,36 @@ const inquirer = require("inquirer");
 const electronHtmlTo = require("electron-html-to");
 
 const questions = [
-  
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "username"
+      },
+      {
+        type: "list",
+        message: "What is your favorite color?",
+        name: "color",
+        choices: [
+            "green",
+            "blue",
+            "pink",
+            "red"
+        ]
+      }
+    ])
+    .then(function(response) {
+      console.log(response.username);
+      console.log(response.color);
+    })
 ];
 
+/*
 function writeToFile(fileName, data) {
  
 }
 
 function init() {
 
-init();
+init(); */

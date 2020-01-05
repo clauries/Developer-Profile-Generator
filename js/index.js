f3565ea046d380b2965421e5ea26b74dc14a2350
clauries <55369403+clauries@users.noreply.github.com>
2020-01-04 22:09:18 -0800

    Completed template literal

--- js/index.js
+++ js/index.js
@@ -40,27 +40,10 @@ const questions = [
             const followers = a.followers;
             const following = a.following;
 
-
-        //console.log(response.username);
-        //console.log(response.color);
-        // console.log(queryUrl);
-        // console.log(favoriteColor);
-        // console.log(name);
-        // console.log(website);
-        // console.log(profileImg);
-        // console.log(bio);
-        // console.log(company);
-        // console.log(location);
-        // console.log("Public Repos:" + publicRepos);
-        // console.log("Followers: " + followers);
-        // console.log("Following: " + following);
-
     });
     axios.get(queryStarred).then(function(res) {
         const a = res.data;
         const starred = a.length;
-
-        //console.log("Stars: " + starred);
 
 });
 })
